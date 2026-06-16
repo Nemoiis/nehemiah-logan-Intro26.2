803e944dccd84184ce7dfde4951642c5a48cf2e1
Nemoiis <nehemiahllogan@gmail.com>
2026-06-16 11:39:00 -0400

    final touches

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Footer
 const body = document.querySelector("body");
 const footer = document.createElement("footer")
 body.appendChild(footer);
@@ -7,7 +8,10 @@ const copyright = document.createElement("p");
 copyright.innerHTML = "&copy; Nehemiah Logan " + thisYear;
 footer.appendChild(copyright);
 
-const skills = ["Data Structs", "Algorithms", "Object Oriented Programming", "Technical Documentation"];
+// Skills
+const skills = ["Data Structs", "Algorithms", "Object Oriented Programming", "Technical Documentation","Python (pandas, matplotlib, SciPy, OOP, web scraping)", 
+    "Statistical Analysis (t-tests, ANOVA)", "Data Visualization", "Microsoft Office Suite",
+    "Google Workspace", "Zoom", "basic computer hardware assembly"];
 const skillsSection = document.getElementById("Skills");
 const skillsList = skillsSection.querySelector("ul");
 
@@ -16,6 +20,8 @@ for (let i = 0; i < skills.length; i++){
     skill.textContent = skills[i];
     skillsList.appendChild(skill);
 }
+
+// Messages
 const messages = document.getElementById("messages");
 
 const messageForm = document.querySelector("[name='leave_message']");
@@ -40,54 +46,53 @@ messageForm.addEventListener("submit", function(event){
     removeButton.innerText = "Remove";
     removeButton.type = "button";
 
+    // Remove button
     removeButton.addEventListener("click", function(event){
         const entry = event.target.parentNode;
         entry.remove();
-    
-        
-    if (messageList.children.length < 1){
-        messages.style.display = "none";
-    }
-    else {messages.style.display = "block";}
-    
+
+        if (messageList.children.length < 1){
+            messages.style.display = "none";
+        }
+        else {messages.style.display = "block";}
     })
 
     newMessage.appendChild(removeButton);
     messageList.appendChild(newMessage);
 
+    // Toggle messages
     if (messageList.children.length < 1){
         messages.style.display = "none";
     }
     else {messages.style.display = "block";}
 
-
     messageForm.reset();
 });
 
+// GitHub repos
 fetch('https://api.github.com/users/Nemoiis/repos')
 .then(function(response){
     return response.json();
 })
-
 .then(function(response){
     const repositories = response;
     console.log(repositories);
 
-const projectSection = document.getElementById("Projects");
-const projectList = projectSection.querySelector('ul');
+    const projectSection = document.getElementById("Projects");
+    const projectList = projectSection.querySelector('ul');
 
-for (let i = 0; i < repositories.length; i++){
-    const project = document.createElement('li');
-    project.classList.add("projects");
-    project.innerHTML = `<a href=${repositories[i].html_url}>${repositories[i].name}</a><p>${repositories[i].description}</p>`
-    projectList.appendChild(project);
-}
+    for (let i = 0; i < repositories.length; i++){
+        const project = document.createElement('li');
+        project.classList.add("projects");
+        project.innerHTML = `<a href=${repositories[i].html_url}>${repositories[i].name}</a><p>${repositories[i].description}</p>`
+        projectList.appendChild(project);
+    }
 })
-
 .catch(function(error){
     console.error("Failed to load repositories:", error);
-  });
+});
 
+// Dark mode
 const toggled = document.getElementById('toggle');
 toggled.addEventListener("change", function() {
     if (toggled.checked) {
@@ -95,5 +100,4 @@ toggled.addEventListener("change", function() {
     } else {
         document.body.classList.remove('light-mode');
     }
-})
-
+});
